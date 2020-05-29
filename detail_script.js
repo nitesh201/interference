959d6ad2d392d820484ce58d0da68d92426d18b1
Nitesh Menon <niteshmenon@gmail.com>
2020-05-28 21:12:34 -0400

    lightbox

--- detail_script.js
+++ detail_script.js
@@ -17,3 +17,20 @@ Array.prototype.forEach.call(
     detailNumbers, 
     element => element.addEventListener("click", detail_click)
 );
+
+var modal = document.getElementById("imageModal");
+var imgs = document.getElementsByClassName("detailImage");
+var modalImg = document.getElementById("modal-image");
+var captionText = document.getElementById("modal-caption");
+
+for (var i = 0; i < imgs.length; i++) {
+  imgs[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+} 
